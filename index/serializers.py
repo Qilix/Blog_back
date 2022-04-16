dcd5c2cdbdcf68d5167df68988858324a5f5959c
Qilix <leevalenti@mail.ru>
2022-04-16 05:50:13 +0300

    edited serializers

--- index/serializers.py
+++ index/serializers.py
@@ -33,4 +33,13 @@ class QuoteSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Quote
+        exclude = ('text',)
+
+class QuoteDetailSerializer(serializers.ModelSerializer):
+    created_at = serializers.DateTimeField(format="%Y-%m-%d %H:%M:%S", required=False, read_only=True)
+    updated_at = serializers.DateTimeField(format="%Y-%m-%d %H:%M:%S", required=False, read_only=True)
+    author = serializers.SlugRelatedField(slug_field="username", read_only=True, many=False)
+
+    class Meta:
+        model = Quote
         fields = '__all__'
