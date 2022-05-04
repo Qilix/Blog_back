9e3398dcb75ee1b44eb0ec37ebfc354832a6ae77
Qilix <leevalenti@mail.ru>
2022-05-04 15:34:41 +0300

    settings

--- index/models.py
+++ index/models.py
@@ -6,10 +6,23 @@ from users.models import User
 class Article(models.Model):
     title = models.CharField(max_length=150)
     text = models.TextField()
-    author = models.ForeignKey(User, on_delete = models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     sub_only = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
+
+class Comment(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    name = models.ForeignKey(User, on_delete=models.CASCADE)
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    active = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['created_on']
+
+    def __str__(self):
+        return 'Comment {} by {}'.format(self.body, self.name)
